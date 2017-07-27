d949f19a83d49cbe98d6bad99747b081ad4f968e
Guillaume Hivert <guillaume.hivert@outlook.com>
2017-07-27 21:46:55 +0200

    Nested records

--- src/Data/Dumper.elm
+++ src/Data/Dumper.elm
@@ -9,12 +9,12 @@ type alias Location =
   { host : String
   , hostname : String
   , protocol : String
-  , origin : String
-  , port_ : String
-  , pathname : String
-  , search : String
-  , hash : String
-  , test : Int
+  , testInt : Int
+  , testFloat : Float
+  , testRecord :
+    { testInside1 : String
+    , testInside2 : String
+    }
   }
 
 dumpLocation : Location -> Html msg
@@ -23,14 +23,18 @@ dumpLocation =
     [ ("host", .host >> dumpString)
     , ("hostname", .hostname >> dumpString)
     , ("protocol", .protocol >> dumpString)
-    , ("origin", .origin >> dumpString)
-    , ("port_", .port_ >> dumpString)
-    , ("pathname", .pathname >> dumpString)
-    , ("search", .search >> dumpString)
-    , ("hash", .hash >> dumpString)
-    , ("test", .test >> dumpInt)
+    , ("testInt", .testInt >> dumpInt)
+    , ("testFloat", .testFloat >> dumpFloat)
+    , ("testRecord", .testRecord >> dumpInsideRecord)
     ]
     "Location"
+
+dumpInsideRecord : { testInside1 : String, testInside2 : String } -> Html msg
+dumpInsideRecord =
+  dumpNestedRecord
+    [ ("testInside1", .testInside1 >> dumpString)
+    , ("testInside2", .testInside2 >> dumpString)
+    ]
 
 
 
@@ -38,12 +42,25 @@ dumpRecord : List (String, a -> Html msg) -> String -> a -> Html msg
 dumpRecord extractors name record =
   extractors
     |> List.map (toTupleRecord record)
-    |> dumpTupleRecords name
+    |> dumpTupleRecords (Just name) 2
     |> Html.div
       [ Html.Attributes.style
         [ ("white-space", "pre")
         , ("font-family", "monospace")
+        , ("background-color", "#F6F8FA")
+        , ("padding", "12px")
+        , ("box-sizing", "border-box")
         ]
+      ]
+
+dumpNestedRecord : List ( String, a -> Html msg ) -> a -> Html msg
+dumpNestedRecord extractors record =
+  extractors
+    |> List.map (toTupleRecord record)
+    |> dumpTupleRecords Nothing 4
+    |> Html.div
+      [ Html.Attributes.style
+        [ ("display", "inline") ]
       ]
 
 dumpString : String -> Html msg
@@ -52,11 +69,11 @@ dumpString string =
 
 dumpInt : Int -> Html msg
 dumpInt int =
-  colorText "#98C379" (toString int)
+  colorText "#D19A66" (toString int)
 
 dumpFloat : Float -> Html msg
 dumpFloat float =
-  colorText "#98C379" (toString float)
+  colorText "#D19A66" (toString float)
 
 
 
@@ -64,21 +81,27 @@ toTupleRecord : a -> (String, a -> Html msg) -> (String, Html msg)
 toTupleRecord record (name, function) =
   (name, function record)
 
-dumpTupleRecords : String -> List (String, Html msg) -> List (Html msg)
-dumpTupleRecords name fields =
+dumpTupleRecords : Maybe String -> Int -> List (String, Html msg) -> List (Html msg)
+dumpTupleRecords name spaces fields =
+  let spaces_ = String.repeat spaces " " in
   case fields of
-    (name, content) :: tail ->
+    (fieldName, content) :: tail ->
       List.concat
-        [ [ colorText "#C376DA" "type alias "
-          , colorText "#D19A66" (name ++ " =\n")
-          , colorText "#C376DA" "  { "
-          , colorText "#5DA6E2" "page "
-          , colorText "#C376DA" ": "
+        [ case name of
+          Just name_ ->
+            [ colorText "#C376DA" "type alias "
+            , colorText "#D19A66" (name_ ++ " =\n")
+            ]
+          Nothing ->
+            [ Html.text "\n" ]
+        , [ colorText "#C376DA" (spaces_ ++ "{ ")
+          , colorText "#5DA6E2" fieldName
+          , colorText "#C376DA" " : "
           , content
           , colorText "#98C379" "\n"
           ]
-        , (List.map dumpField tail)
-        , [ colorText "#C376DA" "  }" ]
+        , (List.map (dumpField spaces) tail)
+        , [ colorText "#C376DA" (spaces_ ++ "}") ]
         ]
 
     [] ->
@@ -92,12 +115,19 @@ colorText color_ text_ =
     ]
     [ Html.text text_ ]
 
-dumpField : (String, Html msg) -> Html msg
-dumpField (name, field) =
-  Html.div []
-    [ colorText "#C376DA" "  , "
-    , colorText "#5DA6E2" (name ++ " ")
-    , colorText "#C376DA" ":"
+dumpField : Int -> (String, Html msg) -> Html msg
+dumpField spaces (name, field) =
+  let spaces_ = String.repeat spaces " " in
+  Html.div [ Html.Attributes.style [ ("diplay", "inline") ] ]
+    [ colorText "#C376DA" (spaces_ ++ ", ")
+    , colorText "#5DA6E2" name
+    , colorText "#C376DA" " : "
     , field
     , Html.text "\n"
     ]
+
+
+main : Html msg
+main =
+  dumpLocation
+    (Location "host_" "hostname_" "protocol_" 0 1.5 { testInside1 = "testInside1_", testInside2 = "testInside2_" })
